d241741c57a197a6c617f46d1609cad8cfbd947b
mjh9401 <mjh9401@naver.com>
2023-08-08 18:19:47 +0900

    MyBatis -> JPA 적용

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,7 @@
 package hello.itemservice;
 
 import hello.itemservice.config.JdbcTemplateV3Config;
+import hello.itemservice.config.JpaConfig;
 import hello.itemservice.config.MyBatisConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +16,7 @@ import javax.sql.DataSource;
 
 
 //@Import(MemoryConfig.class)
-@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 @Slf4j
 public class ItemServiceApplication {
